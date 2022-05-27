81de23b6ed2924b5861817a50082fd9de4cc2bca
Kai Lyons <kai@potabi.com>
2022-05-27 09:01:17 -0600

    Remove null

--- reference.c
+++ reference.c
@@ -8,7 +8,7 @@ int WINAPI WinMain(HINSTANCE hInt, HINSTANCE hPrevInst, LPSTR lpCmdLine,
 #else
 int main() {
 #endif
-	webview_t w = webview_create(0, NULL);
+	webview_t w = webview_create(0, 0);
 	webview_set_title(w, "Webview Example");
 	webview_set_size(w, 480, 320, WEBVIEW_HINT_NONE);
 	webview_navigate(w, "https://en.m.wikipedia.org/wiki/Main_Page");
